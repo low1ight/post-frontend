94df9a12d4dcb51de4f63a8b345081b41e74949e
Maksym <qlowlight@gmail.com>
2026-02-04 11:25:29 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import {Header} from "./shared/ui/Header.tsx";
+
+function App() {
+
+
+  return (
+    <>
+      <Header />
+      <main className="max-w-4xl mx-auto px-4 py-6"> POST</main>
+    </>
+  )
+}
+
+export default App
---
+++ src/shared/ui/Header.tsx
@@ -0,0 +1,13 @@
+
+
+export function Header() {
+    return (
+        <header className="w-full bg-white border-b border-slate-100">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <nav className="h-16 flex items-center justify-between">
+                    <div>Posts Project</div>
+                </nav>
+            </div>
+        </header>
+    )
+}
