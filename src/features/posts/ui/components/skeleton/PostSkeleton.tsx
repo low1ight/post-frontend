08b515b8f5a8a782f5c8d505794d89cf7a556b5f
Maksym <qlowlight@gmail.com>
2026-02-05 13:47:11 +0200

    add PostSkeleton

---
+++ src/features/posts/ui/components/skeleton/PostSkeleton.tsx
@@ -0,0 +1,26 @@
+
+export function PostSkeleton() {
+    return (
+        <div className="card-dark animate-pulse">
+            <div className={"p-10"}>
+                <div>
+                    <div className="h8 bg-slate-700 rounded-md w-3/4 pb-8 mb-5"></div>
+                </div>
+
+                <div>
+                    <div className="h8 bg-slate-700 rounded-md w-full pb-6 mb-2"></div>
+                    <div className="h8 bg-slate-700 rounded-md w-full pb-6 mb-2"></div>
+                    <div className="h8 bg-slate-700 rounded-md w-4/5 pb-6 mb-5"></div>
+                </div>
+
+                <div>
+                    <div className="h8 bg-slate-700 rounded-md w-full pb-5 mb-2"></div>
+                    <div className="h8 bg-slate-700 rounded-md w-full pb-5 mb-2"></div>
+                    <div className="h8 bg-slate-700 rounded-md w-full pb-5 mb-2"></div>
+                    <div className="h8 bg-slate-700 rounded-md w-full pb-5 mb-2"></div>
+                    <div className="h8 bg-slate-700 rounded-md w-1/5 pb-5 mb-5"></div>
+                </div>
+            </div>
+        </div>
+    )
+}
